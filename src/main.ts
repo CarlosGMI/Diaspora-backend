9dc37858a934d403a7bec65df7b13119c16ad0c5
CarlosGMI <carlosmaldonado@correo.unimet.edu.ve>
2021-08-06 20:01:15 -0400

    Set up nest/@config. Add app port to config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('app.port', 3000);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 
 bootstrap();
